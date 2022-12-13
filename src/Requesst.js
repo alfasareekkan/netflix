380784df100a3c0bd4634fa392a1036e35260968
MOHAMMED ALFAS A <87485303+alfasareekkan@users.noreply.github.com>
2022-12-13 07:05:15 +0530

    Update Requesst.js

--- src/Requesst.js
+++ src/Requesst.js
@@ -1,4 +1,4 @@
-const key = '72f7d0516a2b18b27a4a2782260fb811';
+
 const requests = {
     requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
     requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
